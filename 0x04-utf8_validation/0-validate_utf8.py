debd225255207601d1db171eadc389fb34bdfc40
bekatadesse <bekatadesse5@gmail.com>
2023-03-05 07:00:41 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,86 +1,41 @@
 #!/usr/bin/python3
-
 """
-Module 0-validate_utf8
+    UTF8-validator
 """
-
-
-def validUTF8_v1(data):
-    """
-    Determines if a given data set
-    represents a valid UTF-8 encoding
-    """
-    count = 0
-
-    if not data:
-        return False
-
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
-
-        if count == 0:
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                count += 1
-
-            if count == 0:
-                continue
-
-            if count == 1 or count > 4:
-                return False
-        else:
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                return False
-    return count == 0
 
 
 def validUTF8(data):
-    count = 0
+    """
+    Validates whether the given list of integers represents
+    a valid UTF-8 encoding.
 
-    if data is None:
-        return False
+    Args:
+        data (list): A list of integers representing
+        the UTF-8 encoding of a string.
 
+    Returns:
+        bool: True if the input data is a valid UTF-8 encoding,
+              False otherwise.
+    """
+    # Count the number of bytes in the current UTF-8 sequence
+    bytes_to_follow = 0
     for num in data:
-        if count == 0:
-            if num & 128 == 0:
-                count = 0
-            elif num & 224 == 192:
-                count = 1
-            elif num & 240 == 224:
-                count = 2
-            elif num & 248 == 240:
-                count = 3
+        # Check if the current byte is the start of a new UTF-8 sequence
+        if bytes_to_follow == 0:
+            if num >> 5 == 0b110:
+                bytes_to_follow = 1
+            elif num >> 4 == 0b1110:
+                bytes_to_follow = 2
+            elif num >> 3 == 0b11110:
+                bytes_to_follow = 3
+            elif num >> 7 == 0b0:
+                bytes_to_follow = 0
             else:
                 return False
+        # Check if the current byte is a continuation byte
         else:
-            if num & 192 != 128:
+            if num >> 6 != 0b10:
                 return False
-    if count == 0:
-        return True
-    return False
-
-
-def validUTF8_v4(data):
-    n_bytes = 0
-
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
-    for num in data:
-        mask = 1 << 7
-        if n_bytes == 0:
-            while mask & num:
-                n_bytes += 1
-                mask = mask >> 1
-
-            if n_bytes == 0:
-                continue
-
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-        else:
-            if not (num & mask1 and not (num & mask2)):
-                return False
-        n_bytes -= 1
-        return n_bytes == 0
+            bytes_to_follow -= 1
+    # If there are still bytes left to follow, the input is invalid
+    return bytes_to_follow == 0
